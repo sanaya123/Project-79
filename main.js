8bee2dd74ab14eb530ca385976f2173a7abe1559
sanaya123 <83179671+sanaya123@users.noreply.github.com>
2021-12-19 11:59:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 menu_list_array = ["Cheese Pizza", "Pepperoni Pizza","Chicken Tandoori Pizza","Veg Pizza"];
 
 function getmenu(){
-var htmldata;
+var htmldata="";
 menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+ menu_list_array[i] + '<br>'
